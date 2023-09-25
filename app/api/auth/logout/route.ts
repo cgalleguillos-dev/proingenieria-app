c906b52a9ddaa6eaaff6043e7f98240147991024
cgalleguillos-dev <galleguilloscristian2@gmail.com>
2023-09-25 19:54:00 -0300

    refactor(api) remove logout endpoint

--- app/api/auth/logout/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse, NextRequest } from "next/server";
-import { cookies } from 'next/headers';
-
-export const POST = async (req: NextRequest, res: NextResponse) => {
-    cookies().set('authTokens', '', { sameSite: 'strict', httpOnly: true, maxAge: 0 });
-    return NextResponse.redirect(new URL('/login', req.url));
-}
