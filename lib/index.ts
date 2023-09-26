8361fcc458c743934c03c563cf8d382873a75967
cgalleguillos-dev <galleguilloscristian2@gmail.com>
2023-09-26 19:39:07 -0300

    feat(frontend) new function to verify the user session

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,6 @@
 import * as bcrypt from 'bcrypt';
-
+import * as jwt from 'jsonwebtoken';
+import {options} from "preact";
 export const comparePassword = async (
     password: string,
     receivedPassword: string
@@ -9,4 +10,15 @@ export const comparePassword = async (
 
 export const hashPassword = async (password: string): Promise<string> => {
     return await bcrypt.hash(password, 12);
+}
+
+export const decodeToken = (token: string, key: string = 'secret'): any => {
+    return jwt.verify(token, key);
+}
+
+export const verifySession = (session: any, role: string): boolean => {
+    if (!session) {
+        return false;
+    }
+    return session.user.role.name === role;
 }
