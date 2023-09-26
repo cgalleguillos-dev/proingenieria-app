5a779cc6dc8c435cc7aae9a6408b5a4171292dc0
cgalleguillos-dev <galleguilloscristian2@gmail.com>
2023-09-26 19:41:13 -0300

    feat(frontend) get dailyReports to api by user

--- app/ejecutor/formularios/page.tsx
+++ app/ejecutor/formularios/page.tsx
@@ -1,11 +1,33 @@
 import {ExecutorView} from '@/components'
 import {NextPage} from 'next'
+import {getServerSession} from "next-auth";
+import {authOptions} from "@/lib/auth";
+import {headers} from "next/headers";
+import {verifySession} from "@/lib";
+
+const fetchDailyReportsByExecutor = async () => {
+  const res = await fetch('http://localhost:3000/api/dailyreport/byuser',
+    {
+          cache: 'no-cache',
+          headers: headers()
+        }
+  );
+  return await res.json();
+}
 
 interface Props {
 }
 
-const Page: NextPage<Props> = ({}) => {
-  return <ExecutorView/>
+const Page: NextPage<Props> = async ({}) => {
+  const session = await getServerSession(authOptions);
+  if (!verifySession(session, 'Ejecutor')) {
+    return <div>Acceso denegado</div>;
+  }
+
+  const dailyReports = await fetchDailyReportsByExecutor()
+  return <ExecutorView
+    dailyReports={dailyReports}
+  />
 
 }
 
